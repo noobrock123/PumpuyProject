024c04bd67837a63503fd80f7f8e8eb38edc9c49
noobrock123 <garfiwgame@hotmail.com>
2023-03-09 11:05:55 +0700

    Add new url path to login page and add raise exception when user doesn't login first

--- BaseApp/urls.py
+++ BaseApp/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "BaseApp"
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path('login', views.login, name='login'),
+    path('loging_user', views.loging_in, name='loging_in'),
+    path('test', views.test, name='test')
 ]
--- BaseApp/views.py
+++ BaseApp/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'main.html') 
+
+def login(request):
+    return render(request, 'login.html')
+
+def loging_in(request):
+    if (request.method == 'GET'):
+        raise PermissionDenied 
+    else:
+        return redirect(reverse('home'))
+
+
+def test(request):
+    return HttpResponse('BaseApp:home')
