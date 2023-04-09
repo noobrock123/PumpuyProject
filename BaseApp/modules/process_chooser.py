6e438f35b5e388d6e39838e149f1a8c0ddba735c
noobrock123 <garfiwgame@hotmail.com>
2023-04-09 20:41:18 +0700

    Add python3 command in process_chooser

--- BaseApp/modules/process_chooser.py
+++ BaseApp/modules/process_chooser.py
@@ -3,5 +3,5 @@ import os
 class process_chooser():
 
     def yolo_v7(path, video_file):
-        os.system(f"./yolo_v7/detect_and_track.py --source ../{path}")
+        os.system(f"python3 ./yolo_v7/detect_and_track.py --source ../{path}")
         return 0
