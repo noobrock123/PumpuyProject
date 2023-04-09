7062cbdff1cbbe5175933dfe3ee90d5ea248776f
noobrock123 <garfiwgame@hotmail.com>
2023-04-09 21:36:42 +0700

    Add another parameter in video_manager's upload function

--- BaseApp/modules/video_manager.py
+++ BaseApp/modules/video_manager.py
@@ -1,4 +1,4 @@
-from ..models import Video
+from ..models import Video, Intersection
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
 #import modules.processing as p
@@ -6,21 +6,24 @@ from . import scheduler
 
 class video_manager:
     
-    def upload(self, request, name, video_file):
+    def upload(self, request, name, video_file, video_name):
         if request.method != 'POST':
             raise PermissionDenied
         
         data = request.POST
-        path = f"intersectionData/{name}/videos/{video_file}"
+
+        path = f"intersectionData/{video_file}"
         schedule = scheduler.Celery()
+        Video.objects.create(
+            video_name = video_name,
+            length = 300,
+            uploader = request.user,
+            auth_level = 4,
+            intersection = Intersection.objects.get(name=name) 
+        )
         result = schedule.create_job.delay(path, video_file)
         if result:
             print("err")
-        #Video.objects.create(
-        #    video_name = post_data.get('file_name'),
-        #    video_file = request.FILE.get('file'),
-        #    uploader = request.user
-        #)
     
     def download(request):
         if request.method != 'POST':
