2bf6132800a5a9fc5befb887df33bfe5823453b3
noobrock123 <garfiwgame@hotmail.com>
2023-05-09 20:40:15 +0700

    Update process chooser

--- BaseApp/modules/process_chooser.py
+++ BaseApp/modules/process_chooser.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
+from ..models import Video
 
-class process_chooser:
+class process_chooser():
 
-    def yolo_v7(path, video_file):
+    def yolo_v7(path, video_file: Video):
         print('------------------------------')
-        print(path)
-        print(video_file)
+        print(video_file.video_file.name)
         print('------------------------------')
-        # os.system(f"python3 BaseApp/modules/yolo_v7/detect_and_track.py --source BaseApp/{video_file} --loop BaseApp/modules/yolo_v7/loop.json")
+        os.system(f"python3 BaseApp/modules/yolo_v7/detect_and_track.py --source {video_file.video_file.name} --loop BaseApp/modules/yolo_v7/loop.json")
+
         return 0
