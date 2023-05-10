9f895154d2589d9880e42038bd4989c8e13dd9ad
6310682692 <69454013+6310682692@users.noreply.github.com>
2023-05-10 23:30:45 +0700

    try debug docker

--- PumpuyProject/celery.py
+++ PumpuyProject/celery.py
@@ -5,6 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PumpuyProject.settings')
 app = Celery('PumpuyProject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+# app.conf["CELERY_BROKER_URL"] = "redis://localhost:6360"
 
 def debug_task(self):
     print(f'Request: {self.request!r}')
